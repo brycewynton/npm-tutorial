7b91dab2902b80676c510d3b15764e2a79a39c77
Bryce Jensen <brycewjensen@gmail.com>
2020-08-26 14:00:04 -0500

    finished exploring some use cases for npm when developing js for web including momentjs and browserify to quickly bundle js files for web preview

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-var myDate = new Date();
+var moment = require("moment");
 
-console.log(myDate); // prints current date and time to javascript dev console in browser
+var unique = require('uniq');
+
+var myDate = new Date(); // turns out I enjoy the way this classic one looks much more than moment format
+
+var momentFormattedDate = moment(myDate).format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+
+var myList = [1,2,1,1,1,3,3,4,5,6,4,4,7];
+
+var myUniqueList = unique(myList);
+
+
+console.log(momentFormattedDate); // prints current date in moment format from node.js library
+console.log(myUniqueList);
+
 
